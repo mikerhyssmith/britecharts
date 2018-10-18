6b6b704a8c3d221536b5c8faedb12a1e2170818c
Mike Smith <mikerhyssmith@googlemail.com>
2018-10-18 16:35:22 +0100

    improve typings according to typescript styleguide

--- src/charts/bar.d.ts
+++ src/charts/bar.d.ts
@@ -1,4 +1,4 @@
-import { D3Selection } from 'd3-selection';
+import * as D3Selection from 'd3-selection';
 // Type definitions for [~THE LIBRARY NAME~] [~OPTIONAL VERSION NUMBER~]
 // Project: [~THE PROJECT NAME~]
 // Definitions by: [~YOUR NAME~] <[~A URL FOR YOU~]>
@@ -38,65 +38,73 @@ declare function bar(selection: D3Selection): Bar;
  */
 declare namespace Bar {
 
-    export function chartGradient(gradient: string[]): Bar;
+    interface BriteChartsMargin {
+        left: number;
+        right: number;
+        top: number;
+        bottom: number;
+    }
+    type BriteChartsCustomEvent = 'customMouseOver' | 'customMouseMove' | 'customMouseOut' | 'customClick';
+
+    export function chartGradient(x: string[]): Bar;
     export function chartGradient(): string[];
-    export function betweenBarsPadding(padding: number): Bar;
-    export function betweenBarsPadding(module: Bar): number;
+    export function betweenBarsPadding(x: number): Bar;
+    export function betweenBarsPadding(): number;
+    export function colorSchema(x: string[]): Bar;
     export function colorSchema(): string[];
-    export function colorSchema(schema: string[]): Bar;
+    export function enableLabels(x: boolean): Bar;
     export function enableLabels(): boolean;
-    export function enableLabels(enabled: boolean): Bar;
-    export function exportChart(fileName?: string, title?: string): void;
-    export function hasPercentage(hasPercentage: boolean): Bar;
+    export function exportChart(x?: string, title?: string): void;
+    export function hasPercentage(x: boolean): Bar;
     export function hasPercentage(): boolean;
-    export function hasSingleBarHighlight(hasSingleBarHighlight: boolean): Bar;
+    export function hasSingleBarHighlight(x: boolean): Bar;
     export function hasSingleBarHighlight(): boolean;
-    export function height(height: number): Bar;
+    export function height(x: number): Bar;
     export function height(): number;
-    export function highlightBarFunction(highlightFunction: Function<void>): Bar;
+    export function highlightBarFunction(x: Function<void>): Bar;
     export function highlightBarFunction(): Function<void>
-    export function isAnimated(isAnimated: boolean): Bar;
+    export function isAnimated(x: boolean): Bar;
     export function isAnimated(): boolean;
-    export function isHorizontal(isHorizontal: boolean): Bar;
+    export function isHorizontal(x: boolean): Bar;
     export function isHorizontal(): boolean;
-    export function labelsMargin(margin: number): Bar;
+    export function labelsMargin(x: number): Bar;
     export function labelsMargin(): number;
-    export function labelsNumberFormat(format: string): Bar; // Should this use d3-format types ifthey exists
+    export function labelsNumberFormat(x: string): Bar; // Should this use d3-format types ifthey exists
     export function labelsNumberFormat(): string;
-    export function labelsSize(size: number): Bar;
+    export function labelsSize(x: number): Bar;
     export function labelsSize(): number;
-    export function loadingState(markup: string): Bar;
+    export function loadingState(x: string): Bar;
     export function loadingState(): string;
-    export function margin(margin: BriteChartsMargin): Bar;
+    export function margin(x: BriteChartsMargin): Bar;
     export function margin(): BriteChartsMargin;
-    export function nameLabel(label: number): Bar;
+    export function nameLabel(x: number): Bar;
     export function nameLabel(): number; // check this
-    export function numberFormat(format: string): Bar;
+    export function numberFormat(x: string): Bar;
     export function numberFormat(): string;
-    export function on(event: CustomEvent, func: Function<void>): Bar;
-    export function percentageAxisToMaxRatio(axisToMaxRation: number): Bar;
+    export function on(x: CustomEvent, y: Function<void>): Bar;
+    export function percentageAxisToMaxRatio(x: number): Bar;
     export function percentageAxisToMaxRatio(): number;
-    export function shouldReverseColorList(shouldReverseColorList: boolean): Bar;
+    export function shouldReverseColorList(x: boolean): Bar;
     export function shouldReverseColorList(): boolean;
-    export function orderingFunction(func: Function<void>): Bar;
+    export function orderingFunction(x: Function<void>): Bar;
     export function orderingFunction(): Function<void>;
-    export function valueLabel(label: number): Bar;
+    export function valueLabel(x: number): Bar;
     export function valueLabel(): number;
-    export function width(width: number): Bar;
+    export function width(x: number): Bar;
     export function width(): number;
-    export function xAxisLabel(label: string): Bar;
+    export function xAxisLabel(x: string): Bar;
     export function xAxisLabel(): string;
-    export function xAxisLabelOffset(offset: number): Bar;
+    export function xAxisLabelOffset(x: number): Bar;
     export function xAxisLabelOffset(): number;
-    export function xTicks(ticks: number): Bar;
+    export function xTicks(x: number): Bar;
     export function xTicks(): number;
-    export function yAxisLabel(label: string): Bar;
+    export function yAxisLabel(x: string): Bar;
     export function yAxisLabel(): string;
-    export function yAxisLabelOffset(labelOffset: number): Bar;
+    export function yAxisLabelOffset(x: number): Bar;
     export function yAxisLabelOffset(): number;
-    export function yAxisPaddingBetweenChart(yPadding: number): Bar;
+    export function yAxisPaddingBetweenChart(x: number): Bar;
     export function yAxisPaddingBetweenChart(): number;
-    export function yTicks(ticks: number): Bar;
+    export function yTicks(x: number): Bar;
     export function yTicks(): number;
 
 
@@ -106,12 +114,4 @@ declare namespace Bar {
      *~   import f = require('myFuncLibrary');
      *~   console.log(f.defaultName);
      */
-
-    export interface BriteChartsMargin {
-        left: number;
-        right: number;
-        top: number;
-        bottom: number;
-    }
-    export type CustomEvent = 'customMouseOver' | 'customMouseMove' | 'customMouseOut' | 'customClick';
 }
